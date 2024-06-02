54b9f208a8fd6d554fcc563e2c37181314a66caf
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-06-02 15:12:46 +0000

    punto 5

--- papes-canzoniere/src/app/app.component.ts
+++ papes-canzoniere/src/app/app.component.ts
@@ -9,13 +9,15 @@ import { Song } from './song.model';
 export class AppComponent {
 
   canzone !: Song;
+  songList : Song[] = []; 
 
   constructor() {}
 
   aggiungiCanzone(nuovoTitolo: HTMLInputElement, nuovaDurata: HTMLInputElement, nuovaData: HTMLInputElement):boolean {
     console.log(nuovoTitolo.value, nuovaDurata.value, nuovaData.value)
-    
     this.canzone = new Song(nuovaData.value, nuovaDurata.value, nuovoTitolo.value)
+    this.songList.push(this.canzone) // aggiungi la canzone alla lista
+    console.log(this.songList) // visualizziamo la lista
     return false
   }
 }
